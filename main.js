cfa073428a13ce7414c0a8fae7d6c5524d36f04f
Pradipta Dey <pradiptadey@Pradiptas-MacBook-Pro.local>
2026-03-30 15:51:47 +0530

    [#4] |Changes file name from tileData.js to tile_data.js  | Pradipta/Gopi

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { createApp } from "./src/app.js";
 import { Services } from "./src/services.js";
-import { tiles } from "./src/tileData.js";
+import { tiles } from "./src/tile_data.js";
 
 const main = () => {
   const port = Deno.env.get("PORT") || 8000;
