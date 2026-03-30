0fd51e7245ebc6459f63695219aa456216906a6d
Pradipta Dey <pradiptadey@Pradiptas-MacBook-Pro.local>
2026-03-30 17:39:56 +0530

    [#4] | Refactor : generates tile data dynamically using helper function | Pradipta/Gopi
    Replaces static tile array with `createTiles` function
    * Updates `main.js` to generate tiles at runtime
    * Simplifies tile data management by removing hardcoded values
    * Improves scalability and flexibility of tile generation
    * Minor cleanup in board event listener

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import { createApp } from "./src/app.js";
 import { Services } from "./src/services.js";
-import { tiles } from "./src/tile_data.js";
+import { createTiles } from "./src/tile_data.js";
 
 const main = () => {
   const port = Deno.env.get("PORT") || 8000;
+  const tiles = createTiles();
   const service = new Services(tiles);
   const app = createApp(service);
   Deno.serve({ port }, app.fetch);
