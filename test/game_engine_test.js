47e07ee64fe1c6cbf69f925cb25dcf2bf9ce15f6
Pradipta Dey <pradiptadey@Pradiptas-MacBook-Pro.local>
2026-03-31 10:16:16 +0530

    [#6] |Adds method for getting the action after placing a tile| Pradipta/Gopi

--- test/game_engine_test.js
+++ test/game_engine_test.js
@@ -4,7 +4,7 @@ import { GameEngine } from "../src/game_engine.js";
 
 describe("GameEngine", () => {
   let gameEngine;
-  beforeEach(() => gameEngine = new GameEngine());
+  beforeEach(() => (gameEngine = new GameEngine()));
 
   describe("adjacentTiles", () => {
     it("should return adjacent tiles of the tile having 4 adjacent tiles", () => {
@@ -36,6 +36,26 @@ describe("GameEngine", () => {
       const expected = ["2a", "1b"];
 
       assertEquals(gameEngine.adjacentTilesOf("1a"), expected);
+    });
+  });
+
+  describe("actionForPlacingTile", () => {
+    it("the adjacent tiles are not placed", () => {
+      const placedTiles = ["2a", "3a"];
+      const action = gameEngine.actionForPlacingTile("7b", placedTiles);
+      assertEquals(action, "nothing");
+    });
+
+    it("the adjacent tiles are already placed ", () => {
+      const placedTiles = ["2a", "3a"];
+      const action = gameEngine.actionForPlacingTile("1a", placedTiles);
+      assertEquals(action, "build hotel");
+    });
+
+    it("the all adjacent tiles are already placed ", () => {
+      const placedTiles = ["2a", "3a", "1b"];
+      const action = gameEngine.actionForPlacingTile("1a", placedTiles);
+      assertEquals(action, "build hotel");
     });
   });
 });
