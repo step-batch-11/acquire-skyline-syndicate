55ee0fea7d4356acf64f64966ec29936c64823e4
Yash Gairola <yashgairola@Yashs-MacBook-Pro.local>
2026-04-08 17:37:50 +0530

    [#17] | Fixes bug of bonus distribution and stock distribution | yash/pradipta

--- src/models/player.js
+++ src/models/player.js
@@ -92,8 +92,8 @@ export class Player {
   }
 
   sellStocks(hotelName, price) {
+    this.#money = this.#money + price * this.#stocks[hotelName];
     delete this.#stocks[hotelName];
-    this.#money = this.#money + price;
   }
 
   loadGameState(playerDetails) {
